7bb955eef66ec0d8bd962fc7be1e8d1db2613813
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-03-18 02:10:07 +0100

    dodanie komendy define

---
+++ src/commands/Define.ts
@@ -0,0 +1,56 @@
+import {
+  Command, KlasaClient, CommandStore, KlasaMessage,
+} from 'klasa';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { MessageEmbed } from 'discord.js';
+import { IDefinition } from '../interfaces/IDefinition';
+
+export default class DefineCommand extends Command {
+  constructor(client: KlasaClient, store: CommandStore, file: string[], dir: string) {
+    super(client, store, file, dir, {
+      name: 'definiuj',
+      enabled: true,
+      usage: '<query:string>',
+      runIn: ['text'],
+      cooldown: 3600,
+      bucket: 3,
+      description: 'Wyszukuje definicji podanego słowa w serwisie miejski.pl',
+    });
+  }
+
+  private static prepareMessage(defintion: IDefinition) {
+    const messageEmbed = new MessageEmbed();
+    messageEmbed.setURL(defintion.url);
+    messageEmbed.setTitle(`Definicja wyazu ${defintion.query}`);
+    messageEmbed.addField('Definicja', defintion.definition);
+    if (defintion.example.length) {
+      messageEmbed.addField('Przykład', defintion.example);
+    }
+    return messageEmbed;
+  }
+
+  async run(message: KlasaMessage, params: any[]) {
+    const query = params[0] as string;
+    const encodeQuery = encodeURI(query.replace(/ /g, '+'));
+    const url = `https://www.miejski.pl/slowo-${encodeQuery}`;
+    const response = await axios.get(url, { validateStatus: () => true });
+    if (response.status === 404) {
+      message.reply('Nie udało się odnaleźć definicji dla podanego wyrazu!');
+    } else if (response.status === 200) {
+      const $ = cheerio.load(response.data);
+      const definitionNode = $('li', '#words').first();
+      const definition = $('.definition', definitionNode).text().trim();
+      const example = $('.example', definitionNode).text().trim();
+      const definitionMessage = DefineCommand.prepareMessage({
+        url, query, definition, example,
+      });
+      message.reply(definitionMessage);
+    } else {
+      message.reply(`Coś poszło nie tak! ${response.status}`);
+      this.client.console.warn(`Miejski.pl zwrócił status ${response.status}`);
+    }
+
+    return null;
+  }
+}
---
+++ src/interfaces/IDefinition.ts
@@ -0,0 +1,6 @@
+export interface IDefinition {
+  url: string;
+  query: string;
+  definition: string;
+  example: string;
+}
