a80801ddce4b0fba73abe94cb165d7a30040fcf2
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-10-31 15:47:02 +0100

    Create project structure

---
+++ src/Romulus.ConsoleBot/Services/LoggingService.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Romulus.ConsoleBot.Services
+{
+    class LoggingService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly ILogger<LoggingService> _logger;
+
+        public LoggingService(DiscordSocketClient client, CommandService commands, ILogger<LoggingService> logger)
+        {
+            _logger = logger;
+            _client = client;
+            _commands = commands;
+
+            _client.Log += OnLogAsync;
+            _commands.Log += OnLogAsync;
+        }
+
+        private Task OnLogAsync(LogMessage msg)
+        {
+            string logText = $": {msg.Exception?.ToString() ?? msg.Message}";
+
+            switch (msg.Severity.ToString())
+            {
+                case "Critical":
+                    {
+                        _logger.LogCritical(logText);
+                        break;
+                    }
+                case "Warning":
+                    {
+                        _logger.LogWarning(logText);
+                        break;
+                    }
+                case "Info":
+                    {
+                        _logger.LogInformation(logText);
+                        break;
+                    }
+                case "Verbose":
+                    {
+                        _logger.LogInformation(logText);
+                        break;
+                    }
+                case "Debug":
+                    {
+                        _logger.LogDebug(logText);
+                        break;
+                    }
+                case "Error":
+                    {
+                        _logger.LogError(logText);
+                        break;
+                    }
+            }
+
+            return Task.CompletedTask;
+
+        }
+    }
+}
