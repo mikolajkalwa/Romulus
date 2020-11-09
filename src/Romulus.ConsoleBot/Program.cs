4644e442ca336ddd6c40c4e81fc8ba00a0c02d1f
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-11-09 12:23:08 +0100

    Read logger configurationf from appsettings, assign default cultureinfo in main

--- src/Romulus.ConsoleBot/Program.cs
+++ src/Romulus.ConsoleBot/Program.cs
@@ -1,4 +1,8 @@
-﻿using Serilog;
+﻿using System.Globalization;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 using Serilog.Events;
 
 namespace Romulus.ConsoleBot
@@ -7,14 +11,30 @@ namespace Romulus.ConsoleBot
     {
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-           .MinimumLevel.Debug()
-           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-           .Enrich.FromLogContext()
-           .WriteTo.Console()
-           .CreateLogger();
+            var cultureInfo = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
-            Startup.RunAsync(args).GetAwaiter().GetResult();
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+            var configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Startup.RunAsync(configuration).GetAwaiter().GetResult();
+        }
+
+        private static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<Program>()
+                .AddEnvironmentVariables();
         }
     }
 }
